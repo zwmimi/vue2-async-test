759e9f3ca81483b0234aa4895ffec00344e50bfe
zwmimi <zwmimi1212@gmail.com>
2022-11-29 17:43:27 +0900

    fetch関数を実装

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,30 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    posts: [],
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    addPost(state, post) {
+      state.posts.push(post);
+    },
   },
   actions: {
+    fetchPosts({ commit }) {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          data.forEach((post) => {
+            commit("addPost", post);
+          });
+        });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
